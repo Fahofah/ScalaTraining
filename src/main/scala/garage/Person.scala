70ed06995194e9ef0558c18000f144a6a8e1fb33
fahofah <fahriulucaycy@gmail.com>
2017-10-26 15:31:43 +0000

    Organisation clean up

--- src/main/scala/garage/Person.scala
+++ src/main/scala/garage/Person.scala
@@ -4,6 +4,7 @@ abstract class Person(ID: Int,fullName: String) {
 
   private var EmployeeId_itr= 0
   private var CustomerId_itr= 0
+
   def apply(ID: Int, fullName: String): Person = {
     Person.this match {
       case p: Employee => this.ID = EmployeeId_itr + 1
