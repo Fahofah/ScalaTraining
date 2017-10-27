4e3da597d4796fa0f0fa1d5f42b9e676e1bca2c8
fahofah <fahriulucaycy@gmail.com>
2017-10-27 08:04:57 +0000

    Day5 start

--- src/main/scala/garage/Console.scala
+++ src/main/scala/garage/Console.scala
@@ -2,13 +2,17 @@ package garage
 
 object Console extends App {
 
-  val emp1 = new Employee(1,"Mathew Clark", "Welder")
-  val cust1 = new Customer(2,"Marie Sandlers")
+  val emp1 = Employee("Mathew Clark", "Welder")
+  val cust1 = Customer("Marie Sandlers")
+  val emp2 = Employee("Mathew Clark", "Welder")
+  val cust2 = Customer("Marie Sandlers")
   val car1 = new Car(2,"BMW", "i3","Electric","Automatic",5,"12/10/17")
   val bike1 = new Bike(3, "Carrera","on the Go","Pedal","Disk","20/10/17")
 
   println(emp1.toString)
   println(cust1.toString)
+  println(emp2.toString)
+  println(cust2.toString)
   println(car1.toString)
   println(bike1.toString)
 
--- src/main/scala/garage/Customer.scala
+++ src/main/scala/garage/Customer.scala
@@ -1,6 +1,6 @@
 package garage
 
-case class Customer(ID: Int,fullName: String) extends Person(ID,fullName){
+case class Customer(ID: Int,fullName: String) extends Person{
 
   var invoiceReady = false
   var invoicePrice = 0
@@ -9,5 +9,18 @@ case class Customer(ID: Int,fullName: String) extends Person(ID,fullName){
     var status = s"is served and has pending invoice of £$invoicePrice"
     if (!invoiceReady) status = "is waiting service"
     s"\nCustomer $fullName, #$ID $status"
+  }
+}
+
+object Customer {
+  var id = 0
+  private var id_itr = 0
+
+  def apply(fullName: String): Customer = new Customer(getNextId, fullName)
+
+  def getNextId: Int = {
+    id = id_itr + 1
+    id_itr += 1
+    id
   }
 }
